a2fa2a24c46dfa45ee81244da89da53d767750d4
LyamCd <lyam.chardey@gmail.com>
2025-03-10 15:42:11 +0100

    Unit tests for `SolverLSAP`

---
+++ tests/test_solvers.py
@@ -0,0 +1,29 @@
+"""
+Test for LSAP Solvers
+"""
+
+import sys
+from unittest.mock import MagicMock
+
+
+for lib in ("torch", "librariesImport", "gedlibpy", "sinkdiff.sinkdiff", "sinkdiff.sink_utils"):
+    sys.modules[lib] = MagicMock()
+
+import pytest
+np = pytest.importorskip("numpy")
+
+from pyged.solvers import SolverLSAP
+
+def test_solver_lsap():
+    C = np.array([
+        [2, 1, 1, 1, np.inf, np.inf, np.inf],
+        [1, 0, 0, np.inf, 1, np.inf, np.inf],
+        [2, 1, 1, np.inf, np.inf, 1, np.inf],
+        [0, 1, 1, np.inf, np.inf, np.inf, 1],
+        [1, np.inf, np.inf, 0, 0, 0, 0],
+        [np.inf, 1, np.inf, 0, 0, 0, 0],
+        [np.inf, np.inf, 1, 0, 0, 0, 0]
+    ])
+    solver = SolverLSAP()
+    cols, rows = solver.solve(C)
+    assert np.sum(C[rows, cols]) == 2
