8d63b576f77e1bfcc5d294bde40f3bc6db3c1fb5
LyamCd <lyam.chardey@gmail.com>
2025-03-10 15:25:52 +0100

    TODO

---
+++ pyged/__init__.py
@@ -0,0 +1,6 @@
+#  _____ ___  ____   ___  
+# |_   _/ _ \|  _ \ / _ \ 
+#   | || | | | | | | | | |
+#   | || |_| | |_| | |_| |
+#   |_| \___/|____/ \___/ 
+# TODO Ajouter une docstring ici pour résumer la méthode d'approximation & citer les sources (+ lazy imports ?)
